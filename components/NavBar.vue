b847df60ce033ce29455b3889a923ac353f26707
Leonhard Volkmann <leovolkmann@googlemail.com>
2022-06-29 17:03:19 +0200

    refactor: :recycle: initial commit for v2

--- components/NavBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav class="fixed w-full text-xl font-extrabold italic top-4 pl-6 z-30"></nav>
-</template>
-<script>
-export default {
-  name: 'NavBar',
-}
-</script>
-<style lang="postcss"></style>
